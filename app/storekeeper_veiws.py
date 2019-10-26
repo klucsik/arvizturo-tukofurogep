23b348adda755c3d5f7ecc49e98ec8dfb89d3fa6
KlucsikKP <klucsik.krisztian@gmail.com>
2019-10-26 20:41:20 +0200

    heroku deploy rendbetétele, custom veiw

--- app/storekeeper_veiws.py
+++ app/storekeeper_veiws.py
@@ -2,26 +2,23 @@ from app.models import *
 
 from app import storekeeper, db
 from flask_admin.contrib.sqla import ModelView
-
+from flask_admin import BaseView, expose
 '''
 veiws
 '''
 
-
+class UploadProductVeiw(BaseView):
+    @expose('/')
+    def index(self):
+        return 'birb!'
 
 
 '''
 storekeeper interface
 '''
-#
-# storekeeper.add_view(ModelView(User, db.session))
-# storekeeper.add_view(ModelView(Stores, db.session))
+storekeeper.add_view(UploadProductVeiw(endpoint='test'))
+
 # storekeeper.add_view(ModelView(Product, db.session))
 # storekeeper.add_view(ModelView(UseCategory, db.session))
-#
-# storekeeper.add_view(ModelView(Charity, db.session))
-# storekeeper.add_view(ModelView(Chain, db.session))
 # storekeeper.add_view(ModelView(ProductCategory, db.session))
-# storekeeper.add_view(ModelView(Cart, db.session))
-# storekeeper.add_view(ModelView(ConnectCartProduct, db.session))
 # storekeeper.add_view(ModelView(HandlingCategory, db.session))
