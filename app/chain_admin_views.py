09edc347644b8a5c49a08e176609c78ea7851add
vhermecz <vhermecz@gmail.com>
2019-10-27 01:46:21 +0200

    Proper store name rendering on Product admin

--- app/chain_admin_views.py
+++ app/chain_admin_views.py
@@ -10,7 +10,13 @@ under the admin route
 
 chainadmin.add_view(ModelView(User, db.session))
 chainadmin.add_view(ModelView(Stores, db.session))
-chainadmin.add_view(ModelView(Product, db.session))
+
+class ChainAdminProductModelView(ModelView):
+    column_formatters = {
+        "store": lambda v, c, m, p: m.store.store_name
+    }
+
+chainadmin.add_view(ChainAdminProductModelView(Product, db.session))
 chainadmin.add_view(ModelView(UseCategory, db.session))
 
 chainadmin.add_view(ModelView(Charity, db.session))
